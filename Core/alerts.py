a85c2978be01dae205b213f6a91c9377b45f5dd0
nicksweb <nosullivan92@gmail.com>
2020-02-25 06:53:00 +1000

    Starting implementing Pusover for Notifications alongside email alerts which ideally will be configurable through the settings of the application.

--- Core/alerts.py
+++ Core/alerts.py
@@ -1,5 +1,7 @@
 # functions.py
 import globals
+import http.client
+import urllib
 #import functions as f1
 
 #from functions import *
@@ -14,6 +16,20 @@ def sendNotification(title, subject, mailFrom, messageDetail, mailTo):
             "Zone is in alarm state.\n" \
             "" + messageDetail + "" \
             "" + subject + ""
+            
+    conn = http.client.HTTPSConnection("api.pushover.net:443")
+    conn.request("POST", "/1/messages.json",
+      urllib.parse.urlencode({
+        "token": "",                       # Insert app token here
+        "user": "",                       # Insert user token here
+        "html": "1",                                # 1 for HTML, 0 to disable
+        "title": "Motion Detected!",                # Title of the message
+        "message": "<b>Front Door</b> camera!",     # Content of the message
+        "url": "http://IP.ADD.RE.SS",               # Link to be included in message
+        "url_title": "View live stream",            # Text for the link
+        "sound": "siren",                           # Define the sound played
+      }), { "Content-type": "application/x-www-form-urlencoded" })
+    conn.getresponse()
 
     #plaintext = "Alarm Notifcation Test"
 
@@ -23,3 +39,5 @@ def sendNotification(title, subject, mailFrom, messageDetail, mailTo):
 
     #ourmailsender.connect()
     #ourmailsender.send_all()
+    
+    
